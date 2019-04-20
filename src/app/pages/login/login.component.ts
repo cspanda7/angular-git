893eb4a4d7193235b80ddf1d53b7dc49568cfbfb
cspanda7 <cpanda273@gmail.com>
2019-04-21 01:55:58 +0530

    4 files modified

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,5 @@
 import { Component,OnInit } from "@angular/core";
-import { Router, ActivatedRoute } from '@angular/router';
+import { ActivatedRoute,Params,Router } from '@angular/router';
 import {LoginService} from "../../services/login.service";
 
 @Component({
@@ -8,11 +8,23 @@ import {LoginService} from "../../services/login.service";
 })
 export class LoginComponent implements OnInit{
 
- constructor(private router:Router,private loginService:LoginService) {
+ constructor(private router:Router,
+ private route:ActivatedRoute,
+ private loginService:LoginService,
+ ) {
     }
 
 ngOnInit(){
-
+this.route.queryParams.subscribe(
+      (params:Params)=>{
+        console.log(params["code"]);
+        if(params["code"]!=undefined || params["code"]!=null){
+            this.loginService.getAccessToken(params["code"]).subscribe(res => {
+                console.log(res);
+            });
+        }
+      }
+    )
 }
 onSubmit(){
         //this.router.navigate(['/home']);
--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,12 +1,17 @@
 import {Injectable} from '@angular/core';
-import {Headers, Http, Response, RequestOptionsArgs,} from "@angular/http";
+import {Headers, Http, Response, RequestOptionsArgs,RequestOptions} from "@angular/http";
 import {Observable} from "rxjs";
 
 @Injectable()
 export class LoginService {
-
+    private headers: Headers = new Headers();
+    private requestOptions: RequestOptionsArgs = {};
     constructor(private api: Http) {
-
+    this.headers.set("Accept", "application/json");
+    this.headers.set("Content-Type", "application/x-www-form-urlencoded"); 
+ 
+    //this.requestOptions.headers = this.headers;
+    // options = new RequestOptions({ headers: this.headers });
     }
 
     login(): Observable<any> {
@@ -14,6 +19,17 @@ export class LoginService {
         let endPoint = 'https://github.com/login/oauth/authorize';
 
         return this.api.get(endPoint).map(res => res.json() as any).catch(err => Observable.throw(err));
+    }
+    getAccessToken(code): Observable<any> {
+        debugger;
+        let endPoint = 'https://github.com/login/oauth/access_token';
+
+        return this.api.post(endPoint,{
+          'client_id':'19efd6e73e2bb73d0e00',
+          'client_secret':'b93e41565a19fa43158938765756fb0fa4ef7db4',
+          'code':code,
+          'redirect_uri':'https://angular-git.stackblitz.io/login'
+          },new RequestOptions({ headers: this.headers })).map(res => res.json() as any).catch(err => Observable.throw(err));
     }
 
 }
